c70d1845f1a7e8ea4a38178aa13e90cb3b54ddc1
Vini <vinicius.brandaot@gmail.com>
2023-03-02 19:04:54 -0300

    feat: user/me route

--- src/controlers/urls/usersMe.js
+++ src/controlers/urls/usersMe.js
@@ -2,14 +2,18 @@ import { db } from "../../config/database/databaseConnection.js";
 
 export default async function(req, res){
     try {
-        const {token} = res.locals;
-        const user = await db.query(`SELECT * FROM sessions WHERE token = $1`, [token])
-        const {userid} = user.rows[0]
-         
-        const userUrls = await db.query(`SELECT * FROM "shortenUrls" WHERE "userId" = $1`, [userid]);
-        console.log(user.rows)
-        console.log(userid)
-        return res.send(userUrls.rows)
+        const { userId } = res.locals;
+        const user = await db.query(`SELECT * FROM users WHERE id = $1`, [userId])
+
+        const userUrls = await db.query(`SELECT * FROM "shortenUrls" WHERE "userId" = $1`, [userId]);
+        
+        let visitCount = 0
+
+        for(let i=0; i<userUrls.rowCount; i++){
+            visitCount = visitCount += userUrls.rows[i].visitCount
+        }
+        
+        return res.send({id: userId, name: user.rows[0].name, visitCount, shortenedUrls: userUrls.rows})
         
     } catch (error) {
         return res.status(500).send(error.message)
