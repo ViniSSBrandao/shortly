d3c2f04a7647e1b6134b84e3cd6d2f6ccead5106
Vini <vinicius.brandaot@gmail.com>
2023-03-02 14:35:33 +0000

    short url viewcount

---
+++ src/controlers/urls/redirectUrl.js
@@ -0,0 +1,21 @@
+import { db } from "../../config/database/databaseConnection.js";
+
+
+export default async function(req, res){
+    try {
+        const {shortUrl} = req.params;
+
+        const redirectTo = await db.query(`SELECT * FROM "shortenUrls" WHERE "shortenUrl" = $1`, [shortUrl])
+        await db.query('UPDATE "shortenUrls" SET "visits" = $1 WHERE "shortenUrl" = $2;', [redirectTo.rows[0].visits + 1, shortUrl])
+        
+        if (!redirectTo.rowCount){
+            return res.sendStatus(404)
+        }
+
+        
+        return res.redirect(302, redirectTo.rows[0].url)
+        
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
--- src/routers/urlsRouter.js
+++ src/routers/urlsRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import getUrlById from "../controlers/urls/getUrlById.js";
+import redirectUrl from "../controlers/urls/redirectUrl.js";
 import shortenController from "../controlers/urls/shortenController.js";
 import shortenUrlMiddleware from "../middlewares/urls/shorten/shortenUrlMiddleware.js";
 
@@ -9,3 +10,4 @@ export const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", verifySession, shortenUrlMiddleware, shortenController)
 urlRouter.post("/urls/:id", getUrlById)
+urlRouter.post("/urls/open/:shortUrl", redirectUrl)
